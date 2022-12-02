1db61ae9e8c4b83802fdec7c707c7f01256c4ba7
AucT <auct.eu@gmail.com>
2022-12-02 12:24:48 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# b5toast
+b5toast is javascript function to create and show bootstrap 5 toasts.
+
+## Requirements
+
+Add this html to the bottom(or anywhere) on the page. Only id is required. Classes can be changed according to your taste.
+
+```html
+<div class="position-fixed bottom-0 start-0 p-3" id="toast-container"></div>
+<script defer src="/b5toast.js"></script>
+```
+
+## Demo
+[Demo](https://auct.github.io/b5toast/b5toast_demo)
+
+
+## Usage
+
+#### Minimal
+
+```javascript
+b5toast.success('my message'); //show toast success
+b5toast.error('my message'); //show toast danger
+b5toast.show('warning', 'my message'); //show toast with warning/any bootstrap color
+```
+
+#### With optional title
+
+```javascript
+b5toast.success('my message'); //show toast success
+b5toast.error('my message'); //show toast danger
+b5toast.show('warning', 'my message'); //show toast with warning/any bootstrap color
+```
+
+
+#### With optional delay
+Default delay is 10s, can be changed in b5toast.js file
+
+```javascript
+b5toast.success('my message'); //show toast success
+b5toast.error('my message'); //show toast danger
+b5toast.show('warning', 'my message'); //show toast with warning/any bootstrap color
+```
---
+++ b5toast.js
@@ -0,0 +1,40 @@
+//change some settings
+const b5toastContainerElement = document.getElementById("toast-container");
+
+//don't touch code below if you don't know what are you doing
+const b5toast = {
+    delayInMilliseconds: 10000,
+    htmlToElement: function (html) {
+        const template = document.createElement("template");
+        html = html.trim();
+        template.innerHTML = html;
+        return template.content.firstChild;
+    },
+    show: function (color, message, title, delay) {
+        title = title ? title : "";
+        const html = `<div class="toast align-items-center mt-1 text-white bg-${color} border-0" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="d-flex">
+            <div class="toast-body">
+                <b>${title}</b>
+                <div>${message}</div>
+            </div>
+            <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+    </div>`;
+        const toastElement = b5toast.htmlToElement(html);
+        b5toastContainerElement.appendChild(toastElement);
+        const toast = new bootstrap.Toast(toastElement, {
+            delay: delay?delay:b5toast.delayInMilliseconds,
+            animation: true
+        });
+        toast.show();
+        setTimeout(() => toastElement.remove(), delay?delay:b5toast.delayInMilliseconds + 3000); // let a certain margin to allow the "hiding toast animation"
+    },
+
+    error: function (message, title, delay) {
+        b5toast.show("danger", message, title, delay);
+    },
+    success: function (message, title, delay) {
+        b5toast.show("success", message, title, delay);
+    },
+};
